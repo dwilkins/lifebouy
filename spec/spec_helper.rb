d9e39ec051df13c05b2cf3290ccc024fd69aab00
Reed Swenson <rswenson@trinovus.com>
2015-10-09 15:05:29 -0400

    Added gem dependencies to gemspec and configured rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,34 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rspec'
+
+ENV["COVERAGE"] ||= 'off'
+
+if ENV["COVERAGE"] == 'on'
+  require 'simplecov'
+  require 'simplecov-rcov'
+  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+  SimpleCov.start
+end
+
+# load in the support files, if any
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+Rspec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.profile_examples = 10
+  config.mock_with  :rspec
+  config.order      = 'random'
+  config.filter_run :focus => true
+
+  config.run_all_when_everything_filtered = true
+  config.example_status_persistence_file_path = "./spec/examples.txt"
+  Kernel.srand config.seed
+end
